2152ac2e9fdd77614f880f9bffed6418c46b6b3f
RadiantK <pps8853@gmail.com>
2022-06-14 11:08:46 +0900

    로그인용 커맨드 객체 및 로그아웃 구현

---
+++ src/main/java/com/shop/command/LoginCommand.java
@@ -0,0 +1,32 @@
+package com.shop.command;
+
+public class LoginCommand {
+
+	private String email;
+	private String pwd;
+	private int role;
+	
+	public LoginCommand(String email, String pwd, int role) {
+		this.email = email;
+		this.pwd = pwd;
+		this.role = role;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public int getRole() {
+		return role;
+	}
+
+	@Override
+	public String toString() {
+		return "LoginCommand [email=" + email + ", pwd=" + pwd + ", role=" + role + "]";
+	}
+	
+}
