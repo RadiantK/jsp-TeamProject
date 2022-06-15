bf2aa6812a54808250344bf8900b35aa244eaa20
RadiantK <pps8853@gmail.com>
2022-06-15 18:26:23 +0900

    회원 가입 관련 컨트롤러 및 커맨드 객체 생성(회원등록 관련 서비스 객체 구현)

--- src/main/java/com/shop/controller/userController/JoinController.java
+++ src/main/java/com/shop/controller/userController/JoinController.java
@@ -1,4 +1,4 @@
-package com.shop.controller;
+package com.shop.controller.userController;
 
 import java.io.IOException;
 
@@ -8,19 +8,66 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.shop.command.JoinCommand;
+import com.shop.config.ServiceConfig;
+import com.shop.exception.DuplicateMemberException;
+import com.shop.exception.DuplicateNickNameException;
+import com.shop.service.MemberRegistService;
+
 @SuppressWarnings("serial")
-@WebServlet("user/join")
+@WebServlet("/user/join")
 public class JoinController extends HttpServlet {
 	
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		request.getRequestDispatcher("/WEB-INF/page/user/join.jsp").forward(request, response);
 	}
 	
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
+		String email = request.getParameter("email");
+		String pwd = request.getParameter("pwd");
+		String name = request.getParameter("name");
+		String nickName = request.getParameter("nickName");
+		String phone = request.getParameter("phone");
 		
+		String fullAddress = "";
+		String postCode_ = request.getParameter("postcode");
+		if(postCode_ != null && !postCode_.equals("")) fullAddress += postCode_ + " ";
+		
+		String address_ = request.getParameter("address");
+		if(address_ != null && !address_.equals("")) fullAddress += address_;
+		
+		String detailAddress_ = request.getParameter("detailAddress");
+		if(detailAddress_ != null && !detailAddress_.equals("")) fullAddress += " " + detailAddress_;
+		
+		String gender =  request.getParameter("gender");
+		
+		ServiceConfig config = ServiceConfig.getInstance();
+		MemberRegistService service = config.getMemberRegistService();
+		
+		try {
+			JoinCommand jc = new JoinCommand(
+					email, pwd, name, nickName, phone, fullAddress, gender);
+			int n = service.MemberRegist(jc);
+			
+			if(n > 0) {
+				response.sendRedirect(request.getContextPath()+"/user/join/success");
+			}else {
+				response.sendRedirect(request.getContextPath()+"/user/join");
+			}
+		}catch(DuplicateMemberException e) {
+			request.setAttribute("errorEmailMsg", "중복된 이메일을 입력하셨습니다.");
+			request.getRequestDispatcher("/WEB-INF/page/user/join.jsp")
+			.forward(request, response);
+		}catch(DuplicateNickNameException e) {
+			request.setAttribute("errorNickMsg", "중복된 닉네임을 입력하셨습니다.");
+			request.getRequestDispatcher("/WEB-INF/page/user/join.jsp")
+			.forward(request, response);
+		}
 	}
 }
