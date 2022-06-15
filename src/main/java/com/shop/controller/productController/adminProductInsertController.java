5a50f6b61d4064034c3e12898fd8ee4da5eb073a
colmaa <neoncoalmine@gmail.com>
2022-06-15 18:16:14 +0900

    관리자 상품관리, 상품등록 페이지 컨트롤러 생성

---
+++ src/main/java/com/shop/controller/productController/adminProductInsertController.java
@@ -0,0 +1,19 @@
+package com.shop.controller.productController;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/product/insert")
+public class adminProductInsertController extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		request.getRequestDispatcher("/WEB-INF/page/admin/adminProductInsert.jsp")
+		.forward(request, response);
+	}
+}
