cc9592d22958ec84ed544062b87be78cfb502450
seongmin <seongmin@218.38.137.27>
2022-06-16 17:58:34 +0900

    order 컨트롤러 추가 및 수정(장바구니 여러품목/단품 구별)

--- src/main/java/com/shop/controller/ordersController/OrderController.java
+++ src/main/java/com/shop/controller/ordersController/OrderController.java
@@ -1,6 +1,7 @@
 package com.shop.controller.ordersController;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,37 +10,64 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.shop.command.OrderCommand;
 import com.shop.dao.MemberDao;
+import com.shop.dao.ProductDao;
 import com.shop.dto.Member;
-import com.shop.dto.OrderParam;
+import com.shop.dto.Product;
 
 @WebServlet("/orders/order")
 public class OrderController extends HttpServlet{
 
 
+	@SuppressWarnings("unused")
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+										throws ServletException, IOException {
 		
-		// 제품종류 개수
-		// 장바구니에서 주문할 때는 제품종류 개수 파라미터로 전달받고 
-		// 상세페이지에서 바로 주문하면 전달 없이 무조건 한개  
-		int itemCnt = 1;
-		String itemCount = req.getParameter("itemCnt");
-		if (itemCount!=null) itemCnt = Integer.parseInt(itemCount); 
-		System.out.println(itemCnt);
-		
+		// 장바구니에서 주문할 때는 품목수 파라미터로 전달받고 
+		// 상세페이지에서 바로 주문하면 한개  
+		String[] cart = req.getParameterValues("cart");
+		int cartCnt = 1;
+		int itemNum = 0; // 제품번호 
+		int piece = 0; // 구매수량 
 		int totalCnt = 0; // 전체 구매수량
-		int total = 0;
-
-		// 제품이미지, 제품명, 구매수량, 금액 파라미터로 전달 받기 
-		String itemImg = req.getContextPath()+"/images/orderTest.png";
-		String itemName = "두닷 콰트로 에어데스크 1000";
-		int itemPiece = 1;
-			totalCnt += itemPiece;
-		int itemPrice = 10000;
-		int itemCal = itemPiece*itemPrice; // 제품별 합계
-			total += itemCal;
+		int total = 0; // 전체 구매금액 
+		
+		if(cart==null) { // 바로 구매할 때 
+			itemNum = Integer.parseInt(req.getParameter("pnum"));
+			piece = Integer.parseInt(req.getParameter("piece"));
+		} else {
+			cartCnt = cart.length;
+		}
+		
+		System.out.println(cartCnt);
+		
+		// 제품 정보 가져오기 
+		ArrayList<OrderCommand> orderList = new ArrayList<OrderCommand>();
 			
+		for(int i=0; i<cartCnt; i++) {
+			if(cart!=null) {
+				itemNum = Integer.parseInt("cart[i].pnum");
+				piece = Integer.parseInt("cart[i].piece");
+			}
+				
+			ProductDao dao = ProductDao.getInstance();
+			Product vo = dao.selectOne(itemNum);
+			
+			String itemImg = req.getContextPath() + "/images/" + vo.getImage();
+			String itemName = vo.getPname();
+			int price = vo.getPrice();
+			int discount = vo.getDiscount();
+			int discountPrice = price*(100-discount)/100;
+			totalCnt += piece;
+			int itemTotal = discountPrice*piece;
+			total += itemTotal;
+				
+			OrderCommand orderProduct = 
+						new OrderCommand(itemNum,itemImg,itemName,price,discount,discountPrice,piece,itemTotal);
+			orderList.add(orderProduct);
+		}			
 		
 		// 세션아이디 있으면 회원번호, 이름, 전화번호 정보 가져오기 
 		String mnum = "";
@@ -51,16 +79,20 @@ public class OrderController extends HttpServlet{
 
 		if(id!=null) {
 			MemberDao dao = MemberDao.getInstance();
-			Member vo = dao.selectOne(id);			
-			mnum = vo.getMemberNum();
-			mname = vo.getName();
-			phone = vo.getPhone();
+			Member member = dao.selectOne(id);			
+			mnum = member.getMemberNum();
+			mname = member.getName();
+			phone = member.getPhone();
 		}
-		
-		OrderParam orderParam = new OrderParam(itemCnt,itemImg,itemName,itemPiece,itemPrice,itemCal,totalCnt,total,mnum,mname,phone);
-		
+	
 		resp.setContentType("text/plain;charset=utf-8");
-		req.setAttribute("orderParam",orderParam);
+		req.setAttribute("orderList", orderList);
+		req.setAttribute("cartCnt", cartCnt);
+		req.setAttribute("totalCnt", totalCnt);
+		req.setAttribute("total", total);
+		req.setAttribute("mnum", mnum);
+		req.setAttribute("mname", mname);
+		req.setAttribute("phone", phone);
 		req.getRequestDispatcher("/WEB-INF/page/orders/order.jsp").forward(req, resp);
 
 	}
