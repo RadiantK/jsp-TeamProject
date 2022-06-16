15ab4b824ae3c3c0b402f92d4bca9e28e799506d
RadiantK <pps8853@gmail.com>
2022-06-16 18:10:37 +0900

    서비스 설정 클래스 비회원 로그인을 위한 서비스 객체 추가

--- src/main/java/com/shop/config/ServiceConfig.java
+++ src/main/java/com/shop/config/ServiceConfig.java
@@ -1,25 +1,31 @@
 package com.shop.config;
 
 import com.shop.dao.MemberDao;
+import com.shop.dao.OrdersDao;
 import com.shop.service.MemberLoginService;
 import com.shop.service.MemberRegistService;
+import com.shop.service.NoneMemberLoginService;
 
 public class ServiceConfig {
 
 	private MemberDao memberDao;
+	private OrdersDao ordersDao;
 	
 	private MemberRegistService memberRegistService;
 	private MemberLoginService memberLoginService;
+	private NoneMemberLoginService noneMemberLoginService;
 	
 	private static ServiceConfig serviceConfig = null;
 	
 	private ServiceConfig() {
 		// dao 초기화
 		memberDao = MemberDao.getInstance();
+		ordersDao = OrdersDao.getInstance();
 		
 		// 서비스 초기화
 		memberRegistService = new MemberRegistService(memberDao);
 		memberLoginService = new MemberLoginService(memberDao);
+		noneMemberLoginService = new NoneMemberLoginService(ordersDao);
 	}
 	
 	public static ServiceConfig getInstance() {
@@ -29,16 +35,16 @@ public class ServiceConfig {
 		return serviceConfig;
 	}
 
-	public MemberDao getMemberDao() {
-		return memberDao;
-	}
-
 	public MemberRegistService getMemberRegistService() {
 		return memberRegistService;
 	}
 
 	public MemberLoginService getMemberLoginService() {
 		return memberLoginService;
+	}
+
+	public NoneMemberLoginService getNoneMemberLoginService() {
+		return noneMemberLoginService;
 	}
 
 }
