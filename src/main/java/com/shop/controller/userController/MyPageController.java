a9374fad02fa73f257f961b1c2592b407aca49af
RadiantK <pps8853@gmail.com>
2022-06-16 09:09:00 +0900

    마이페이지 프로필 처리

--- src/main/java/com/shop/controller/userController/MyPageController.java
+++ src/main/java/com/shop/controller/userController/MyPageController.java
@@ -7,6 +7,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.shop.dao.MemberDao;
+import com.shop.dto.Member;
 
 @SuppressWarnings("serial")
 @WebServlet("/user/mypage/profile")
@@ -15,6 +19,13 @@ public class MyPageController extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		HttpSession session = request.getSession();
+		String email = (String)session.getAttribute("sessionId");
+		
+		MemberDao memberDao = MemberDao.getInstance();
+		Member member = memberDao.selectOne(email);
+		request.setAttribute("m", member);
+		
 		request.getRequestDispatcher("/WEB-INF/page/mypage/mypage.jsp")
 		.forward(request, response);
 	}
