7734eb78e47d35e540414f355edfaa66c5bfc84a
RadiantK <pps8853@gmail.com>
2022-06-17 17:04:32 +0900

    회원 탈퇴 기능 구현 ajax를 사용한 처리

--- src/main/java/com/shop/controller/userController/DeleteMemberController.java
+++ src/main/java/com/shop/controller/userController/DeleteMemberController.java
@@ -7,6 +7,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.shop.dao.MemberDao;
 
 @SuppressWarnings("serial")
 @WebServlet("/user/mypage/delmem")
@@ -17,5 +20,25 @@ public class DeleteMemberController extends HttpServlet{
 
 		request.getRequestDispatcher("/WEB-INF/page/mypage/deleteMember.jsp")
 		.forward(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String pwd = request.getParameter("pwd");
+		
+		HttpSession session = request.getSession();
+		String email = (String) session.getAttribute("sessionId");
+		
+		MemberDao memberDao = MemberDao.getInstance();
+		int n = memberDao.withdraw(email, pwd);
+		
+		if(n > 0) {
+			response.sendRedirect(request.getContextPath()+"/user/logout");
+		}else {
+			request.setAttribute("errorMsg", "회원탈퇴중 오류가 발생했습니다.");
+			request.getRequestDispatcher("/WEB-INF/page/mypage/deleteMember.jsp")
+			.forward(request, response);
+		}
 	}
 }
