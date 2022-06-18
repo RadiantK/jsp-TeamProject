9f91634ced776d0b5c92e93d6a9b5cc6a31bb9cc
RadiantK <pps8853@gmail.com>
2022-06-18 15:25:33 +0900

    관리자용 회원 목록 컨트롤러 생성

---
+++ src/main/java/com/shop/controller/AdminUserListController.java
@@ -0,0 +1,10 @@
+package com.shop.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@SuppressWarnings("serial")
+@WebServlet("/admin/user/list")
+public class AdminUserListController extends HttpServlet{
+
+}
