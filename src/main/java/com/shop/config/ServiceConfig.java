a103e499f8f1c13831a5e7de3ec79fbe2230cab8
RadiantK <pps8853@gmail.com>
2022-06-20 18:20:59 +0900

    serviceConfig 장바구니 관련 서비스 객체 추가

--- src/main/java/com/shop/config/ServiceConfig.java
+++ src/main/java/com/shop/config/ServiceConfig.java
@@ -1,8 +1,12 @@
 package com.shop.config;
 
+import com.shop.dao.CartDao;
+import com.shop.dao.CartDetailDao;
 import com.shop.dao.MemberDao;
 import com.shop.dao.OrdersDao;
+import com.shop.dao.ProductDao;
 import com.shop.service.KakaoLoginService;
+import com.shop.service.MemberCartAddService;
 import com.shop.service.MemberEditService;
 import com.shop.service.MemberLoginService;
 import com.shop.service.MemberRegistService;
@@ -12,12 +16,16 @@ public class ServiceConfig {
 
 	private MemberDao memberDao;
 	private OrdersDao ordersDao;
+	private ProductDao productDao;
+	private CartDao cartDao;
+	private CartDetailDao cartDetailDao;
 	
 	private MemberRegistService memberRegistService;
 	private MemberLoginService memberLoginService;
 	private NoneMemberLoginService noneMemberLoginService;
 	private MemberEditService memberEditService;
 	private KakaoLoginService kakaoLoginService;
+	private MemberCartAddService memberCartAddService;
 	
 	private static ServiceConfig serviceConfig = null;
 	
@@ -25,6 +33,9 @@ public class ServiceConfig {
 		// dao 초기화
 		memberDao = MemberDao.getInstance();
 		ordersDao = OrdersDao.getInstance();
+		productDao = ProductDao.getInstance();
+		cartDao = CartDao.getInstance();
+		cartDetailDao = CartDetailDao.getInstance();
 		
 		// 서비스 초기화
 		memberRegistService = new MemberRegistService(memberDao);
@@ -32,6 +43,9 @@ public class ServiceConfig {
 		noneMemberLoginService = new NoneMemberLoginService(ordersDao);
 		memberEditService = new MemberEditService(memberDao);
 		kakaoLoginService = new KakaoLoginService(memberDao);
+		memberCartAddService = new MemberCartAddService(
+				cartDao, cartDetailDao, productDao, memberDao);
+				
 	}
 	
 	public static ServiceConfig getInstance() {
@@ -59,6 +73,10 @@ public class ServiceConfig {
 
 	public KakaoLoginService getKakaoLoginService() {
 		return kakaoLoginService;
+	}
+
+	public MemberCartAddService getMemberCartAddService() {
+		return memberCartAddService;
 	}
 	
 }
