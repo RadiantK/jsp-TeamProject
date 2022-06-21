2f68a6dd7b1d07f9c49d367951fedd0e6036f647
RadiantK <pps8853@gmail.com>
2022-06-21 14:24:21 +0900

    장바구니 추가 관련 처리

--- src/main/java/com/shop/controller/CartInsertController.java
+++ src/main/java/com/shop/controller/CartInsertController.java
@@ -1,6 +1,9 @@
 package com.shop.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +11,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.json.JSONObject;
+
+import com.shop.config.ServiceConfig;
+import com.shop.exception.CartException;
+import com.shop.service.MemberCartAddService;
 
 @SuppressWarnings("serial")
 @WebServlet("/user/cart")
@@ -17,18 +26,55 @@ public class CartInsertController extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		// 상품번호
 		String productNum_ = request.getParameter("pnum");
 		int productNum = 0;
 		if(productNum_ != null && !productNum_.equals("")) 
 			productNum = Integer.parseInt(productNum_);
 		
+		// 세션에서 이메일 얻어오기
 		HttpSession session = request.getSession();
 		String email = (String) session.getAttribute("sessionId");
 		
-		if(email != null) {
+		// 서비스 객체 생성
+		ServiceConfig config = ServiceConfig.getInstance();
+		MemberCartAddService service = config.getMemberCartAddService();
+		
+		JSONObject jo = new JSONObject();
+		if(email != null) { // 회원일 때 장바구니
+			try {
+				// 장바구니에 상품추가
+				int n = service.addCart(productNum, email);
+				if(n > 0) {
+					jo.put("code", true);
+				}else {
+					jo.put("code", false);
+				}
+			}catch(CartException e) {
+				jo.put("code", "error");
+			}
+		}else { // 비회원일 때 장바구니
+			List<Integer> cart = (List<Integer>) session.getAttribute("cart");
+			//List<ProductCommand> cart = new ArrayList<>();
+			if(cart == null) {
+				List<Integer> list = new ArrayList<>();
+//				NonMemberCartCommand command = new NonMemberCartCommand(
+//						p.getProductNum(), p.getPname(), p.getImage(),
+//						p.getPrice(), p.getCnt());
+				list.add(productNum);
+				session.setAttribute("cart", list);
+				cart = new ArrayList<>(list);
+			}else {
+				cart.add(productNum);
+			}
+			if(cart != null) {
+				jo.put("code", true);
+			}
 			
-		}else {
-			
+			System.out.println(cart);
 		}
+		response.setContentType("text/plain; charset=utf-8");
+		PrintWriter pw = response.getWriter();
+		pw.print(jo);
 	}
 }
