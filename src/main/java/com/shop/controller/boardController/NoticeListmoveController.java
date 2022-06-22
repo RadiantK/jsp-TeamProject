6840258c522219060cbf7a9460cd092112d4a9e7
홍민기 <alsrl1406@naver.com>
2022-06-22 15:23:49 +0900

    공지사항 페이지 JSON 처리로 인한 컨트롤러 추가

---
+++ src/main/java/com/shop/controller/boardController/NoticeListmoveController.java
@@ -0,0 +1,18 @@
+package com.shop.controller.boardController;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/board/Notice/Listmove")
+public class NoticeListmoveController extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/page/board/Notice_List.jsp").forward(req, resp);
+	}
+
+}
